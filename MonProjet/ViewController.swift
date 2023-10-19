9704d8a6f3a1f0d6693cd9bf9bc6763cb4b67257
etudiant <etudiant@ccm2-air14.local>
2023-10-19 17:39:35 +0200

    cours 1 gestion auth

--- MonProjet/ViewController.swift
+++ MonProjet/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onClickGetStarted(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = pwdTextField.text else {
+            // Gérer le cas où l'email ou le mdp est vide
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "An error has occurred.", message: error.localizedDescription, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                print("L'utilisateur est connecté!")
+                let tasksPage = UIStoryboard(name: "Main", bundle: nil)
+                if let tasksPageViewController = self.storyboard?.instantiateViewController(withIdentifier: "TasksPageViewController") {
+                    self.navigationController?.pushViewController(tasksPageViewController, animated: true)
+                }
+            }
+        }
+    }
 }
-
